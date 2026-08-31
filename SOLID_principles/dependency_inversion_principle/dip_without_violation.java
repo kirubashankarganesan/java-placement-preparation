eeb624eea60d5a1785a8aef069707ff7429d95c1
kirubashankarganesan <kirubashankarg2005@gmail.com>
2026-08-31 19:59:01 +0530

    first commit

--- SOLID_principles/dependency_inversion_principle/dip_without_violation.java
+++ SOLID_principles/dependency_inversion_principle/dip_without_violation.java
@@ -1,38 +1,40 @@
 package SOLID_principles.dependency_inversion_principle;
 
-
-interface keyboard{
+interface keyboard {
     void connect();
 }
 
-class wiredkeyboard implements keyboard{
-    public void connect(){
+class wiredkeyboard implements keyboard {
+    public void connect() {
         System.out.println("wired connected");
     }
 }
-class wiredlesskeyboard implements keyboard{
-    public void connect(){
+
+class wiredlesskeyboard implements keyboard {
+    public void connect() {
         System.out.println("wireless connected");
     }
 }
 
-class computer{
+class computer1 {
     private keyboard keyboard;
 
-    public computer(keyboard keyboard){
-        this.keyboard=keyboard;
+    public computer1(keyboard keyboard) {
+        this.keyboard = keyboard;
     }
-    public void start(){
+
+    public void start() {
         keyboard.connect();
     }
 }
+
 public class dip_without_violation {
     public static void main(String[] args) {
-        keyboard wired=new wiredkeyboard();
-        computer pc1=new computer(wired);
+        keyboard wired = new wiredkeyboard();
+        computer1 pc1 = new computer1(wired);
         pc1.start();
-        keyboard wiredless=new wiredkeyboard();
-        computer pc2=new computer(wiredless);
+        keyboard wireless = new wiredlesskeyboard();
+        computer1 pc2 = new computer1(wireless);
         pc2.start();
     }
 }
